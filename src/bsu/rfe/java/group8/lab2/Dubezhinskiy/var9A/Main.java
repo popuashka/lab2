93ecf2c380be532aab4a478f8b2137835d022b82
popuashka <dubezhinskiy96@mail.ru>
2020-12-10 19:33:09 +0300

    сделал

---
+++ src/bsu/rfe/java/group8/lab2/Dubezhinskiy/var9A/Main.java
@@ -0,0 +1,15 @@
+package bsu.rfe.java.group8.lab2.Dubezhinskiy.var9A;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //создаем экзкмпляр фрейма
+        Formula frame = new Formula();
+        //создаем реакцию на нажатие закрытия фрейма
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //показываем фрейм на экране
+        frame.setVisible(true);
+    }
+}
